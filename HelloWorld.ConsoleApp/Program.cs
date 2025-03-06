8da1d235f68e361d0b463b05ace9468f1a209867
Bernardo dos Santos Ferreira <bernardosantospg@gmail.com>
2025-03-06 16:13:42 -0300

    teste

--- HelloWorld.ConsoleApp/Program.cs
+++ HelloWorld.ConsoleApp/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("HOlá Mundo");
+            Console.WriteLine("Olá Mundo");
             Console.ReadLine();
         }
     }
